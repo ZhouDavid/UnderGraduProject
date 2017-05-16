c908a39d9a6e10473ebeadf10532750c43833576
Jianyu Zhou <Jianyu Zhou>
2017-05-16 23:44:47 +0800

    update

--- code/practice/tensorflow_tutorial/Partial_Differential_Equations.py
+++ code/practice/tensorflow_tutorial/Partial_Differential_Equations.py
@@ -30,33 +30,33 @@ for n in range(40):
 	u_init[a,b] = np.random.uniform()
 	ut_init[a,b] = u_init[a,b]
 print (ut_init)
-# DisplayArray(u_init, rng=[-0.1, 0.1])
+DisplayArray(u_init, rng=[-0.1, 0.1])
 
 
-# Parameters:
+#Parameters:
 # eps -- time resolution
 # damping -- wave damping
-# eps = tf.placeholder(tf.float32, shape=())
-# damping = tf.placeholder(tf.float32, shape=())
+eps = tf.placeholder(tf.float32, shape=())
+damping = tf.placeholder(tf.float32, shape=())
 
-# # Create variables for simulation state
-# U  = tf.Variable(u_init)
-# Ut = tf.Variable(ut_init)
+# Create variables for simulation state
+U  = tf.Variable(u_init)
+Ut = tf.Variable(ut_init)
 
-# # Discretized PDE update rules
-# U_ = U + eps * Ut
-# Ut_ = Ut + eps * (laplace(U) - damping * Ut)
+# Discretized PDE update rules
+U_ = U + eps * Ut
+Ut_ = Ut + eps * (laplace(U) - damping * Ut)
 
-# # Operation to update the state
-# step = tf.group(
-#   U.assign(U_),
-#   Ut.assign(Ut_))
+# Operation to update the state
+step = tf.group(
+  U.assign(U_),
+  Ut.assign(Ut_))
 
-# # Initialize state to initial conditions
-# tf.global_variables_initializer().run()
+# Initialize state to initial conditions
+tf.global_variables_initializer().run()
 
-# # Run 1000 steps of PDE
-# for i in range(1000):
-#   # Step simulation
-#   step.run({eps: 0.03, damping: 0.04})
-#   DisplayArray(U.eval(), rng=[-0.1, 0.1])
+# Run 1000 steps of PDE
+for i in range(1000):
+  # Step simulation
+  step.run({eps: 0.03, damping: 0.04})
+  DisplayArray(U.eval(), rng=[-0.1, 0.1])
