9a26fadd4d83c2ba173523863e1c690949ffd445
ZhouDavid <zjybox@126.com>
2017-06-06 19:19:54 +0800

    add trans

--- code/translate.py
+++ code/translate.py
@@ -2,6 +2,13 @@
 import hashlib
 import urllib
 import requests
+import chardet
+import string
+
+def cleanText(rawOriginText):
+	f = filter(lambda x:x in string.printable,rawOriginText)
+	cleanOiriginText = [item for item in f]
+	return str(cleanOiriginText)
 
 def md5(str):
 	m = hashlib.md5()
@@ -16,10 +23,15 @@ def translate(origin):
 	key = '3Lp28tN3wuavY4K8LET2'
 	sign = (appid+q+salt+key).encode('utf-8')
 	sign = md5(sign)
-	print('translating:q=',q)
+	# print('translating:q=',q)
 	q = urllib.parse.quote(q)
 	url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q='+q+'&from=en&to=zh&appid='+appid+'&salt='+salt+'&sign='+sign
 	url = url.encode('utf-8')
 	req = requests.get(url=url)
 	trans = req.text
 	return trans
+if __name__ == '__main__':
+	originText = open('origin-rnet.txt','r',encoding='utf-8').read()
+	originText = cleanText(originText)
+	#transText = translate(originText).encode('latin-1').decode('unicode_escape').encode('utf-8')
+	open('trans-rnet.txt','w').write(originText)
